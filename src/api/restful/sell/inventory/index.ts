3fb1082b29f47e307a70b5d78d81141cd8acaa6e
Sehar Abbas <sehar.abbas@conradlabs.com>
2022-01-05 15:54:57 +0300

    removed junk code

--- src/api/restful/sell/inventory/index.ts
+++ src/api/restful/sell/inventory/index.ts
@@ -266,11 +266,6 @@ export default class Inventory extends Restful {
         format,
         limit,
         offset,
-        "bleh":"bleh"
-      },
-      headers: {
-        "Accept-Language": acceptLanguage?? "en-US",
-        "Accept-Things": acceptLanguage ?? "en-US",
       },
     });
   }
